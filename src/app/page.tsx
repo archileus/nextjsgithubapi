49de7649dde821cefbbc0283bc904006d81a876c
Ardeus <lie_suhendra@msn.com>
2023-07-18 15:14:51 +0700

    fix: use lg responsive

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ const SEARCH_FRAME_COUNT = 3;
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col p-16">
-      <div className={`grid grid-cols-1 gap-16 md:grid-cols-3 md:gap-8`}>
+      <div className={`grid grid-cols-1 gap-16 lg:grid-cols-3 lg:gap-8`}>
         {Array.from(Array(SEARCH_FRAME_COUNT).keys()).map(index => <SearchUser key={index} />)}
       </div>
     </main>
